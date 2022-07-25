ea79fd87f875b903cb8f2139f7318f57484b7455
econom1st <camcycamovu4@gmail.com>
2022-07-25 20:28:31 +0300

    edit code

--- startJolokia.sh
+++ startJolokia.sh
@@ -4,4 +4,4 @@ pid=`ps aux | grep java | grep mock | awk '{print $2}'`
 
 echo "pid = $pid"
 
-java -jar stub-jolokia/jolokia-jvm-1.7.1-agent.jar start $pid
+nohup java -jar stub-jolokia/jolokia-jvm-1.7.1-agent.jar start $pid &
